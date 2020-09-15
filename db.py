9efccd4f79a23ed6ee618752b25518cdd934360e
yume_yu <yu.matchang@gmail.com>
2020-09-16 01:58:16 +0900

    add funcs for temp_logged
    救援ID一時記憶のためのdb操作関数を追加

--- db.py
+++ db.py
@@ -8,6 +8,8 @@ from contextlib import closing
 DBPATH = "gbf_search.sqlite"
 GET_BOSS_CATTEGORIRS = "SELECT * FROM boss_categories ORDER BY id ASC"
 GET_BOSSLIST_BY_ID = "SELECT * FROM boss_name WHERE category = ?;"
+LOG_BATTLEID_QUERY = "insert into temp_logged values (?,?)"
+CLEAR_LOG_QUERY = "delete from temp_logged"
 
 
 def get_bosscategories():
@@ -19,7 +21,7 @@ def get_bosscategories():
         list: [{'id': 1, 'category_name': 'name'}, ...]
 
     Returns:
-        sqlite3.OperationalError: SQLに関連するすべてのエラー
+        sqlite3.OperationalError: SQLファイルが見つからないとき
     """
     with closing(sqlite3.connect(DBPATH)) as connection:
         cursor = connection.cursor()
@@ -42,10 +44,9 @@ def get_bosslist_by_id(id: int):
         list: [{'id': 0, 'boss_name': 'name', 'search_query': 'query'}, ...]
 
     Returns:
-        sqlite3.OperationalError: SQLに関連するすべてのエラー
+        sqlite3.OperationalError: SQLファイルが見つからないとき
     """
     with closing(sqlite3.connect(DBPATH)) as connection:
-        connection = sqlite3.connect(DBPATH)
         cursor = connection.cursor()
         params = (id,)
         cursor.execute(GET_BOSSLIST_BY_ID, params)
@@ -60,6 +61,42 @@ def get_bosslist_by_id(id: int):
             }
         )
     return bossdata_list
+
+
+def clear_logged_battle_id():
+    """clear_logged_battle_id
+
+    一時記憶テーブルに記錄された救援IDをすべて削除する
+
+    Returns:
+        sqlite3.OperationalError: SQLファイルが見つからないとき
+    """
+    with closing(sqlite3.connect(DBPATH)) as connection:
+        cursor = connection.cursor()
+        cursor.execute(CLEAR_LOG_QUERY)
+        connection.commit()
+
+
+def log_battle_id(battle_id: str, tweet_date: str):
+    """log_battle_id
+
+    使用する救援IDをDBに記錄する。
+
+    Args:
+        battle_id(str): 記錄する救援ID
+        tweet_date(str): 対象のIDが記錄されていたtweetが投稿されていた時間
+    Return:
+        bool: 重複せず記錄できたか
+    """
+    with closing(sqlite3.connect(DBPATH)) as connection:
+        cursor = connection.cursor()
+        try:
+            params = (battle_id, tweet_date)
+            cursor.execute(LOG_BATTLEID_QUERY, params)
+            connection.commit()
+            return True
+        except sqlite3.IntegrityError:
+            return False
 
 
 if __name__ == "__main__":
