f3d64060a0bf6492d48b52c844bc1d61cb82c14e
yume_yu <yu.matchang@gmail.com>
2020-09-16 01:39:57 +0900

    add doc and refactor

--- db.py
+++ db.py
@@ -1,17 +1,30 @@
-import sqlite3
+"""db
 
-# データベースファイルのパス
+sqlite3のデータベースを使う部分の関数をまとめたモジュール
+"""
+import sqlite3
+from contextlib import closing
+
 DBPATH = "gbf_search.sqlite"
 GET_BOSS_CATTEGORIRS = "SELECT * FROM boss_categories ORDER BY id ASC"
-GET_BOSSLIST_BY_ID = "SELECT boss_name.id,boss_name.name,boss_name.search_query FROM boss_name LEFT OUTER JOIN boss_categories ON boss_name.category = boss_categories.id WHERE boss_categories.id = ?;"
+GET_BOSSLIST_BY_ID = "SELECT * FROM boss_name WHERE category = ?;"
 
 
 def get_bosscategories():
-    connection = sqlite3.connect(DBPATH)
-    cursor = connection.cursor()
-    cursor.execute(GET_BOSS_CATTEGORIRS)
-    # 全件取得は cursor.fetchall()
-    data = cursor.fetchall()
+    """get_bosscategories
+
+    データベースからマルチボスカテゴリをすべて取得する
+
+    Returns:
+        list: [{'id': 1, 'category_name': 'name'}, ...]
+
+    Returns:
+        sqlite3.OperationalError: SQLに関連するすべてのエラー
+    """
+    with closing(sqlite3.connect(DBPATH)) as connection:
+        cursor = connection.cursor()
+        cursor.execute(GET_BOSS_CATTEGORIRS)
+        data = cursor.fetchall()
     categories = []
     for category in data:
         categories.append({"id": category[0], "category_name": category[1]})
@@ -19,11 +32,24 @@ def get_bosscategories():
 
 
 def get_bosslist_by_id(id: int):
-    connection = sqlite3.connect(DBPATH)
-    cursor = connection.cursor()
-    params = (id,)
-    cursor.execute(GET_BOSSLIST_BY_ID, params)
-    data = cursor.fetchall()
+    """get_bosslist_by_id
+
+    データベースから指定されたカテゴリのマルチボスをすべて取得する
+
+    Args:
+        id(int): 取得するボスのカテゴリーid
+    Returns:
+        list: [{'id': 0, 'boss_name': 'name', 'search_query': 'query'}, ...]
+
+    Returns:
+        sqlite3.OperationalError: SQLに関連するすべてのエラー
+    """
+    with closing(sqlite3.connect(DBPATH)) as connection:
+        connection = sqlite3.connect(DBPATH)
+        cursor = connection.cursor()
+        params = (id,)
+        cursor.execute(GET_BOSSLIST_BY_ID, params)
+        data = cursor.fetchall()
     bossdata_list = []
     for boss_list in data:
         bossdata_list.append(
