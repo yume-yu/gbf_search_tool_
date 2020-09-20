30b1f2b8955f5ceeb9c794c485285a439d183095
yume_yu <yu.matchang@gmail.com>
2020-09-20 20:08:00 +0900

    update select menu with scroll
    ボス選択画面の表示を画面サイズが合わないときでもスクロールできるように修正

--- view.py
+++ view.py
@@ -5,8 +5,17 @@ import db
 from util import gbss_addstr
 
 locale.setlocale(locale.LC_ALL, "")
+
+MAIN_WIN_WIDTH = 80
+TOP_PART_HEIGHT = 4
+MIDDLE_PART_HEIGHT = 11
+BOTTOM_PART_HEIGHT = 4
+MAIN_WIN_HEIGHT = TOP_PART_HEIGHT + MIDDLE_PART_HEIGHT + BOTTOM_PART_HEIGHT
+
 CANCEL = -1
-TITLE = "title"
+TITLE = "Select Search Target"
+CATEGORY = "< CATEGORY >"
+NAME = "< NAME >"
 HOWTOCONTROL_TEXT = "↑↓:up&down →:ENTER ←:CANCEL"
 
 
@@ -25,17 +34,24 @@ def boss_select_menu(stdscr: curses.window):
     # 必要だから書く
     curses.start_color()
     # 色の設定
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLUE)
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLUE)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
     # バックグラウンド設定
     stdscr.bkgd(" ", curses.color_pair(1) | curses.A_BOLD)
     # 画面のマックスサイズをタプルで取得
     y, x = stdscr.getmaxyx()
     # windowの生成
-    window = stdscr.derwin(int(y) - 4, int(x / 2), 3, 0)
+    window = stdscr.derwin(
+        MIDDLE_PART_HEIGHT, int(MAIN_WIN_WIDTH / 2), TOP_PART_HEIGHT, 0
+    )
     # window2の生成
-    window2 = stdscr.derwin(int(y) - 4, int(x / 2), 3, int(x - (x / 2)))
+    window2 = stdscr.derwin(
+        MIDDLE_PART_HEIGHT,
+        int(MAIN_WIN_WIDTH / 2),
+        TOP_PART_HEIGHT,
+        int(MAIN_WIN_WIDTH / 2),
+    )
     # window2の背景の設定
     window.bkgd(" ", curses.color_pair(2))
     # window1の背景の設定
@@ -45,15 +61,17 @@ def boss_select_menu(stdscr: curses.window):
     window2.border()
 
     categories = db.get_bosscategories()
-    stdscr.addstr(1, int(x / 2) - 3, TITLE, curses.A_REVERSE)
-    stdscr.addstr(int(y - 1), int(x / 2) - 10, HOWTOCONTROL_TEXT, curses.A_REVERSE)
+    stdscr.addstr(1, int((MAIN_WIN_WIDTH - len(TITLE)) / 2), TITLE, curses.A_BOLD)
+    stdscr.addstr(TOP_PART_HEIGHT - 1, 1, CATEGORY, curses.A_BOLD)
+    stdscr.addstr(TOP_PART_HEIGHT - 1, int(MAIN_WIN_WIDTH / 2) + 1, NAME, curses.A_BOLD)
+    stdscr.addstr(MAIN_WIN_HEIGHT - 2, 1, HOWTOCONTROL_TEXT, curses.A_REVERSE)
     stdscr.refresh()
     while True:
-        selected = menu(window, categories, "category_name")
+        selected = scrolled_menu(window, categories, "category_name")
         if selected == CANCEL:
             continue
         bosslists = db.get_bosslist_by_id(selected + 1)
-        selected = menu(window2, bosslists, "boss_name")
+        selected = scrolled_menu(window2, bosslists, "boss_name")
         if selected == CANCEL:
             continue
         return bosslists[selected]
@@ -97,5 +115,146 @@ def menu(window: curses.window, datas: list, tag: str):
             return CANCEL
 
 
+def scrolled_menu(window: curses.window, datas: list, tag: str):
+    """menu
+
+    cursesを使って引数のリストを表示してユーザーに選択を促す。
+
+    Args:
+        window: リストを表示/操作するcurses.Windowクラス
+        datas: 列挙する内容1つづつをdictとして保持したリスト
+        tag: datasのデータ1つの中で、表示する内容のkey
+    Returns:
+        int: ユーザーがリストの何番目を選択したのか値
+    """
+    window.erase()
+    window.border()
+
+    # スクロール準備
+    height, width = window.getmaxyx()
+    window.scrollok(True)
+    window.idlok(True)
+    window.setscrreg(1, height - 1)
+    top = 0
+    bottom = height - 2
+    label_offset = 4
+
+    window.keypad(True)
+    selected = 0
+    maxnum = len(datas) - 1
+
+    # 初期出力
+
+    for index, data in enumerate(datas):
+        if top <= index <= bottom - 1:
+            if selected == index:
+                window.addstr(
+                    index + 1,
+                    label_offset,
+                    "".join([" " for index in range(width - 2 - label_offset)]),
+                    curses.A_REVERSE,
+                )
+                gbss_addstr(
+                    window, index + 1, label_offset - 1, data[tag], curses.A_REVERSE
+                )
+            else:
+                gbss_addstr(window, index + 1, label_offset, data[tag])
+
+    if bottom < len(datas):
+        gbss_addstr(window, bottom - top, 1, "↓")
+    if top > 0:
+        gbss_addstr(window, top + 1, 1, "↑")
+
+    while True:
+        inputkey = window.getch()
+        if inputkey == curses.KEY_UP:
+            if selected > 0:
+                window.addstr(
+                    selected - top + 1, 1, "".join([" " for index in range(width - 2)])
+                )
+                gbss_addstr(
+                    window, selected - top + 1, label_offset, datas[selected].get(tag)
+                )
+                selected = selected - 1
+                if top > selected:
+                    top = top - 1
+                    bottom = bottom - 1
+                    window.scroll(-1)
+                    window.border()
+                window.addstr(
+                    selected - top + 1, 1, "".join([" " for index in range(width - 2)])
+                )
+                window.addstr(
+                    selected - top + 1,
+                    label_offset,
+                    "".join([" " for index in range(width - 2 - label_offset)]),
+                    curses.A_REVERSE,
+                )
+                gbss_addstr(
+                    window,
+                    selected - top + 1,
+                    label_offset - 1,
+                    datas[selected].get(tag),
+                    curses.A_REVERSE,
+                )
+                if bottom < maxnum + 1:
+                    gbss_addstr(window, bottom - top, 1, "↓")
+                if top > 0:
+                    gbss_addstr(window, 1, 1, "↑")
+                window.refresh()
+        elif inputkey == curses.KEY_DOWN:
+            if selected < maxnum:
+                window.addstr(
+                    selected - top + 1, 1, "".join([" " for index in range(width - 2)])
+                )
+                gbss_addstr(
+                    window, selected - top + 1, label_offset, datas[selected].get(tag)
+                )
+                selected = selected + 1
+                if bottom <= selected:
+                    top = top + 1
+                    bottom = bottom + 1
+                    window.scroll(1)
+                    window.border()
+                window.addstr(
+                    selected - top + 1, 1, "".join([" " for index in range(width - 2)])
+                )
+                window.addstr(
+                    selected - top + 1,
+                    label_offset,
+                    "".join([" " for index in range(width - 2 - label_offset)]),
+                    curses.A_REVERSE,
+                )
+                gbss_addstr(
+                    window,
+                    selected - top + 1,
+                    label_offset - 1,
+                    datas[selected].get(tag),
+                    curses.A_REVERSE,
+                )
+                if bottom < maxnum + 1:
+                    gbss_addstr(window, bottom - top, 1, "↓")
+                if top > 0:
+                    gbss_addstr(window, 1, 1, "↑")
+                window.refresh()
+        elif inputkey == curses.KEY_RIGHT:
+            return selected
+        elif inputkey == curses.KEY_LEFT:
+            window.erase()
+            window.border()
+            window.refresh()
+            return CANCEL
+
+
 if __name__ == "__main__":
-    print(curses.wrapper(boss_select_menu))
+    main_height = TOP_PART_HEIGHT + MIDDLE_PART_HEIGHT + BOTTOM_PART_HEIGHT
+
+    def main(stdscr):
+        curses.curs_set(False)
+        curses.resize_term(main_height, MAIN_WIN_WIDTH)
+        select = boss_select_menu(stdscr)
+        curses.curs_set(True)
+
+        return select
+
+    print(curses.wrapper(main))
